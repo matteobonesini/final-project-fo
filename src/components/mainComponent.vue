6affbef38cd7b38d908a48a0d14a525fd75f6cbd
FraFra2 <kekkino41@gmail.com>
2023-10-11 18:12:15 +0200

    ADD - Pages, Components

---
+++ src/components/mainComponent.vue
@@ -0,0 +1,19 @@
+<script>
+import { RouterView } from "vue-router";
+export default {
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  components: {
+    RouterView,
+  },
+};
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/pages/developerPage.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: "",
+    };
+  },
+};
+</script>
+
+<template>
+  Template
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped></style>
