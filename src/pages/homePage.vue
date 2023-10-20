77139a2ce5ea45aa0dd0b9422c635a3ba9ff2411
matteobonesini <130700773+matteobonesini@users.noreply.github.com>
2023-10-20 13:16:50 +0200

    ADD - review page

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -7,7 +7,7 @@ export default {
         return {
             workFields: [],
             workField: 'null',
-            developers: []
+            developers: null
         };
     },
     created() {
@@ -59,7 +59,7 @@ export default {
                     dove gli sviluppatori creano il tuo futuro.</p>
                 <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                     <select id="countries" v-model="workField"
-                        class="w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        class="w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option selected value="null">Cerca uno sviluppatore per categoria</option>
                         <option v-for="workField in workFields" :value="workField.id">{{ workField.name }}</option>
                     </select>
@@ -74,7 +74,7 @@ export default {
 
         <section class="mt-10">
             <h5 class="text-center text-black dark:text-white text-3xl font-bold">Scopri i servizi dei nostri sponsor</h5>
-            <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 py-10">
+            <div v-if="developers" class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 py-10">
                 <router-link :to="{ name: 'developer', params: { id: developer.user.id } }" class="mx-auto"
                     v-for="developer in developers" href="">
                     <img :src="developer.full_img_src" class="rounded-full h-56 w-56 object-cover">
@@ -82,6 +82,20 @@ export default {
                             class="bg-[--primary] text-white text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-[--dark-primary] dark:text-zinc-950 leading-5 ms-5">S</span>
                     </p>
                 </router-link>
+            </div>
+            <div v-else class="flex items-center justify-center w-full h-56">
+                <div role="status">
+                    <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor" />
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill" />
+                    </svg>
+                    <span class="sr-only">Loading...</span>
+                </div>
             </div>
         </section>
     </div>
--- src/pages/messagePage.vue
+++ src/pages/messagePage.vue
@@ -15,7 +15,6 @@ export default {
     },
     methods: {
         sendMessage() {
-
             axios
                 .post(`http://127.0.0.1:8000/api/message`, {
                     'developer_id': this.$route.params.id,
@@ -25,12 +24,14 @@ export default {
                     'content': this.content
                 })
                 .then(response => {
+                    this.error = false;
                     this.success = true;
                     setTimeout(() => {
                         this.$router.push({ name: 'developer', params: { id: this.$route.params.id } })
                     }, 5000);
                 })
                 .catch((error) => {
+                    this.success = false;
                     this.error = true;
                 })
         }
@@ -41,9 +42,11 @@ export default {
 <template>
     <div class="container px-5 w-full md:w-3/5  mx-auto">
         <form @submit.prevent="sendMessage()" class="pt-10">
+            <h2 class="text-xl font-bold mb-5 text-[--tertiary] dark:text-[--dark-tertiary]">Manda un messaggio a {{
+                this.$route.query.dev }}</h2>
             <div v-if="error" class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
                 role="alert">
-                Errore nell'invio del messaggio! Riprova più tardi.
+                Errore nell'invio del messaggio! Controlla i campi del form e riprova.
             </div>
             <div v-if="success"
                 class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
@@ -51,33 +54,37 @@ export default {
                 Inviato con successo!
             </div>
             <div class="mb-6">
-                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</label>
-                <input v-model="name" type="text" id="name"
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"><span
+                        class="text-red-500 text-lg me-1">*</span>Nome</label>
+                <input v-model="name" type="text" id="name" maxlength="255"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required>
             </div>
             <div class="mb-6">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"><span
+                        class="text-red-500 text-lg me-1">*</span>Email</label>
                 <input v-model="email" type="email" id="email"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required>
             </div>
             <div class="mb-6">
-                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titolo
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"><span
+                        class="text-red-500 text-lg me-1">*</span>Titolo
                     messaggio</label>
-                <input v-model="title" type="text" id="name"
+                <input v-model="title" type="text" id="name" maxlength="64"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required>
             </div>
             <div class="mb-6">
-                <label for="content" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contenuto
+                <label for="content" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"><span
+                        class="text-red-500 text-lg me-1">*</span>Contenuto
                     messaggio</label>
-                <textarea v-model="content" id="content" rows="4"
+                <textarea v-model="content" id="content" rows="4" maxlength="2048"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Lascia un messaggio..." required></textarea>
             </div>
             <button type="submit"
-                class="text-white bg-[--tertiary] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-[--tertiary] ">Submit</button>
+                class="text-white bg-[--tertiary] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-[--tertiary] ">Invia</button>
         </form>
     </div>
 </template>
